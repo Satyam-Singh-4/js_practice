b57a8a661e88d6a83a8652f8494fa4a7b1a2a951
Satyam-Singh-4 <satyam2707singh@gmail.com>
2023-04-05 14:18:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,316 @@
+// document.querySelectorAll(".btn").forEach((item) => {
+//   item.addEventListener("click", () => {
+//     alert("welcome...!");
+//   });
+// });
+
+// document.write("hello world");
+// alert("hello people");
+// console.log("hello world .....!");
+
+//Variables
+//Variable-Most Basic Building Block
+//Variables-Store,Access,Modify==Value
+//Declare,Assignment Operator,Assign Value
+
+let name = 'Satyam Singh';
+//some code here
+console.log(name);
+//variable declartion
+
+let address, zip, state;
+address = '6/3/97/2 banjara hills';
+zip = '500004';
+state = 'TS';
+name = 'Satyam Singh 1';
+console.log(address, zip, state);
+console.log(name);
+
+const f_Name = 'Arun';
+const l_Name = 'Sharma';
+
+const fullName = `your full_name is :${f_Name} ${l_Name}`;
+console.log(fullName);
+
+let arr = [1, 2, 3, 4, 5];
+let sum = arr.reduce((acc, value) => acc + value, 0);
+console.log(sum);
+
+const n = 's';
+console.log(typeof n);
+
+let arr1 = [
+  {
+    name: 'Satyam',
+    age: 20,
+  },
+  {
+    name: 'Satyam',
+    age: 21,
+  },
+];
+arr1.forEach((object) => delete object.age);
+console.log(arr1);
+
+function duplicateCharCount(str) {
+  if (str) {
+    var obj = {};
+    for (let i = 0; i < str.length; i++) {
+      console.log([str[i]]);
+      if (obj[str[i]]) {
+        obj[str[i]] += str[i];
+      } else {
+        obj[str[i]] = 1;
+      }
+    }
+    console.log(obj);
+  }
+}
+
+duplicateCharCount('aabcdd');
+
+//Function expression
+const add = function (num1, num2) {
+  return num1 + num2;
+};
+let result = add(4, 7);
+console.log(result);
+
+//Function challenge
+//c-1
+const calculateTotal = (subTotal, tax) => subTotal + tax;
+let res = calculateTotal(20, 10);
+console.log(res);
+
+const object1 = {
+  a: 'somestring',
+  b: 42,
+  c: false,
+};
+
+console.log(Object.values(object1));
+console.log(Object.getPrototypeOf(arr1));
+
+//Objects
+
+const person = {
+  name: 'Arun',
+  lastName: 'S',
+  age: 27,
+  education: true,
+  married: true,
+  sibling: ['anna', 'susan', 'peter'],
+  greeting(name) {
+    console.log(`hello my name is  ${name}`);
+  },
+};
+console.log(person.name);
+console.log(person.sibling[1]);
+person.greeting('john');
+
+function phoneticLookup(val) {
+  let result = '';
+
+  // Only change code below this line
+  var lookup = {
+    alpha: 'Adams',
+    bravo: 'Boston',
+    charlie: 'Chicago',
+    delta: 'Denver',
+    echo: 'Easy',
+    foxtrot: 'Frank',
+  };
+
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+console.log(phoneticLookup('charlie'));
+
+//remove duplicate from array
+const a = [1, 2, 3, 4, 5, 3];
+let b = [];
+
+for (let index = 0; index < a.length; index++) {
+  b.indexOf(a[index]) === -1
+    ? b.push(a[index])
+    : console.log('duplicate', a[index]);
+}
+
+console.log('b is =', b);
+
+//What is the output
+let c = [1, 2, 3];
+let d = c.map((num) => {
+  c.push(num + 3);
+  return num + 1;
+});
+console.log(c, d);
+
+//Local scope
+//can not be access from outside code blocks
+//if - NOT VAR
+
+let name1 = 'bobo';
+
+function calculate() {
+  const name = 'john';
+  const age = 25;
+  lastName = 'singh';
+}
+calculate();
+console.log(lastName);
+if (true) {
+  const name = 'john';
+}
+console.log(`my name is ${name1} and I am awesome`);
+
+//variable lookup
+
+const globalNumber = 20;
+
+function sumOfNumber(n1, n2) {
+  const sum = n1 + n2 + globalNumber;
+  console.log(sum);
+}
+
+sumOfNumber(3, 4);
+
+//map
+//does return a new array
+//does not change size of original array
+//uses value of original array when making new one
+
+const people = [
+  {
+    name: 'Satyam',
+    age: 22,
+    position: 'developer',
+  },
+  {
+    name: 'Arun',
+    age: 24,
+    position: 'sr.developer',
+  },
+  {
+    name: 'Saurabh',
+    age: 25,
+    position: 'Hr',
+  },
+];
+
+const res1 = people.map((person) => {
+  return `<h1>${person.name}<h1>`;
+});
+
+console.log(res1);
+
+const newPeople = people.map((person) => {
+  return {
+    firstName: person.name,
+    newAge: person.age + 10,
+  };
+});
+console.log(newPeople);
+
+document.body.innerHTML = res1.join('');
+
+//filter
+//does return new array
+//can manipulate the size of new  array
+//returns based on   condition
+
+const peopless = [
+  {
+    name: 'Satyam',
+    age: 24,
+    position: 'developer',
+    salary: 400,
+  },
+  {
+    name: 'Arun',
+    age: 24,
+    position: 'sr.developer',
+    salary: 600,
+  },
+  {
+    name: 'Saurabh',
+    age: 25,
+    position: 'Hr',
+    salary: 1000,
+  },
+];
+
+const p = peopless.filter((person) => person.age < 25);
+console.log(p);
+
+//find
+//return object
+//return first match ,if not found return undefined
+//great for getting unique value
+
+const obj = peopless.find((person) => person.age === 24);
+console.log(obj);
+console.log(peopless[0].name);
+
+//reduce
+//iterates,callback function
+//reduces to a single value -objcet,array,numbers
+//1 parametre('acc') - total of all calculations
+//2 parametre('curr') - current iteration/value
+
+const totalSpend = peopless.reduce(
+  (acc, currentItem) => (acc += currentItem.salary),
+  0
+);
+console.log(totalSpend);
+
+//Array challenges
+
+console.log(students);
+
+//let e = ['a\0b'];
+
+// let newStudents = students.map((data, idx) => {
+//   return {
+//     ...data,
+//     role: 'student',
+//     idxId: idx,
+//   };
+// });
+
+// newStudents[3].role = 'employee';
+
+// console.log(newStudents);
+
+const updatedStudents = students.map((student) => {
+  student.role = 'student';
+  return student;
+});
+console.log(updatedStudents);
+
+const newStudents = students.filter((element) => {
+  // if (element.score >= 80) {
+  //   return (element.passingYear = 2022);
+  // }
+  return element.score >= 80
+    ? (element.passingYear = 2023)
+    : element.passingYear;
+});
+console.log(newStudents);
+
+const findStudent = students.find((student) => {
+  return student.score > 80;
+});
+console.log(findStudent);
+
+const avgScore =
+  students.reduce((totalScore, student) => {
+    return totalScore + student.score;
+  }, 0) / students.length;
+console.log(avgScore);
+
+//Math objec
+//Standard built in object-always available
+
---
+++ data.js
@@ -0,0 +1,32 @@
+const students = [
+  {
+    id: 1,
+    name: 'Bob',
+    score: 80,
+    favoriteSubject: 'math',
+  },
+  {
+    id: 2,
+    name: 'Peter',
+    score: 70,
+    favoriteSubject: 'physics',
+  },
+  {
+    id: 3,
+    name: 'John',
+    score: 84,
+    favoriteSubject: 'chemistry',
+  },
+  {
+    id: 4,
+    name: 'Smith',
+    score: 60,
+    favoriteSubject: 'science',
+  },
+  {
+    id: 5,
+    name: 'Wood',
+    score: 55,
+    favoriteSubject: 'english',
+  },
+];
